86241ad9cbbb78d387eb094ce6f5d44c8f71608d
Marcelo <marcelo@siasp.com.br>
2022-08-07 22:44:38 -0300

    Adicionando then

--- getCat/assets/js/script.js
+++ getCat/assets/js/script.js
@@ -3,6 +3,14 @@ const catBtn = document.getElementById("change-cat");
 const catImg = document.getElementById("cat");
 
 const getCats = async () => {
+    const data = await fetch(BASE_URL)
+    .then(res=> res.json())
+    .catch(e=> console.log(e));
+
+    return data.webpurl;
+};
+
+const getCat = async () => {
   try {
     const data = await fetch(BASE_URL);
     const json = await data.json();
